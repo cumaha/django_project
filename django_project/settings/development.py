b0db41ac94aacd19f6431845070462e5b623ee13
Randall Degges <rdegges@gmail.com>
2010-11-07 16:30:13 -0800

    Adding a default sqlite3 db for development.
    I always use SQLite3 for development. Why? Because it is simple and requires no
    external setup.
    
    All test db's will go into SITE_ROOT/db/site_name.db. That's a good convention
    for the default database.
    
    If you add other databases (for multi-db stuff), then name those appropriately,
    eg: SITE_ROOT/db/users.db, etc.

--- django_project/settings/development.py
+++ django_project/settings/development.py
@@ -3,3 +3,15 @@
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+## Database settings.
+DATABASES = {																	
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': SITE_ROOT / 'db' / PROJECT_NAME+'.db',
+		'USER': '',
+		'PASSWORD': '',
+		'HOST': '',
+		'PORT': '',
+	}
+}
