5ad182ead670e2a4ab005fc8c462fd43be6389ba
Randall Degges <rdegges@gmail.com>
2011-03-29 18:20:12 -0700

    Removing cruft.

--- django_project/settings/prod.py
+++ django_project/settings/prod.py
@@ -4,26 +4,6 @@
 from common import *
 
 
-## Debug.
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-SEND_BROKEN_LINK_EMAILS = True
-
-## Email.
-EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = ''
-#EMAIL_HOST_PASSWORD = ''
-#EMAIL_PORT = 25
-#EMAIL_USE_TLS = True
-EMAIL_SUBJECT_PREFIX = '[%s] ' % PROJECT_NAME
-
-## Site admins.
-ADMINS = (
-	('Randall Degges', 'rdegges@gmail.com'),
-)
-MANAGERS = ADMINS
-
-## Database settings.
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -34,16 +14,3 @@ DATABASES = {
 		'PORT': '',						 # Set to empty string for default. Not used with sqlite3.
 	}
 }
-
-## Cache.
-CACHE_BACKEND = 'file://%s' % (SITE_ROOT / 'cache' / PROJECT_NAME)
-
-## Key.
-try:
-	SECRET_KEY = open(SECRET_FILE).read().strip()
-except IOError:
-	try:
-		with open(SECRET_FILE, 'w') as f:
-			f.write(gen_secret_key(50))
-	except IOError:
-		raise Exception('Cannot open file %s for writing.' % SECRET_FILE)
