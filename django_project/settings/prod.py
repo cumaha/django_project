b4c6a16c58575c9867d8f88dfe042d7eb1999c09
Randall Degges <rdegges@gmail.com>
2011-03-29 18:21:36 -0700

    Adding block comments.

--- django_project/settings/prod.py
+++ django_project/settings/prod.py
@@ -4,6 +4,19 @@
 from common import *
 
 
+########## EMAIL CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ''
+EMAIL_PORT = 25
+EMAIL_SUBJECT_PREFIX = '[%s->STAGING] ' % SITE_NAME
+EMAIL_USE_TLS = False
+SERVER_EMAIL = 'root@localhost'
+########## END EMAIL CONFIGURATION
+
+
+########## DATABASE CONFIGURATION
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -14,3 +27,4 @@ DATABASES = {
 		'PORT': '',						 # Set to empty string for default. Not used with sqlite3.
 	}
 }
+########## END DATABASE CONFIGURATION
